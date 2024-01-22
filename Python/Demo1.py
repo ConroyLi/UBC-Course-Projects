f9a50e0876f89a08ac87753c05c5afcb713e170d
Conroy <Conroycong@outlook.com>
2024-01-22 13:56:02 -0800

    Updated

--- Python/Demo1.py
+++ Python/Demo1.py
@@ -1,3 +1,4 @@
+"""
 import math
 import numpy as np
 
@@ -16,3 +17,29 @@ A_svd= U@S_m@Vh
 #print(np.allclose(A,A_svd))
 print(S)
 print(S_m)
+"""
+import numpy as np
+
+# Specify the size of the matrix
+n = 5
+m = 8
+
+# Create a random (m, n) matrix
+matrix_A = np.random.randint(0, 9, size=(m, n))
+
+# Perform SVD
+U, singular_values, Vh = np.linalg.svd(matrix_A)
+
+# Construct the singular value matrix
+S_m = np.zeros((U.shape[1], Vh.shape[0]))
+np.fill_diagonal(S_m, singular_values)
+
+# Reconstruct matrix from SVD
+A_svd = U @ S_m @ Vh
+
+# Output
+print("Original Matrix A:\n", matrix_A)
+print("Reconstructed Matrix from SVD:\n", A_svd)
+print("Singular Values:\n", singular_values)
+print("Diagonal Matrix of Singular Values:\n", S_m)
+print("SVD Reconstruction Close to Original:", np.allclose(matrix_A, A_svd))
