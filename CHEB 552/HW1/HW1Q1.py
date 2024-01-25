f3514d7374a5b46c253c4dd98f1c9592b372fb03
Conroy <conroycong@outlook.com>
2024-01-24 20:27:05 -0800

    Updated 1.24

--- CHEB 552/HW1/HW1Q1.py
+++ CHEB 552/HW1/HW1Q1.py
@@ -1,4 +1,4 @@
-from sympy import symbols, diff, solve, hessian, Matrix
+from sympy import symbols, diff, solve, hessian, Matrix, latex
 
 # Define symbols
 x1, x2, x = symbols('x1 x2 x')
@@ -9,6 +9,7 @@ F_a = (x1 - 2)**4 + (x1 - 2*x2)**2
 # Partial derivatives for Function a
 partial_F_a_x1 = diff(F_a, x1)
 partial_F_a_x2 = diff(F_a, x2)
+
 
 # Stationary points for Function a
 stationary_points_a = solve((partial_F_a_x1, partial_F_a_x2), (x1, x2))
@@ -32,7 +33,6 @@ F_c_prime = diff(F_c, x)
 # Stationary points for Function c
 stationary_points_c = solve(F_c_prime, x)
 
-print(stationary_points_a, stationary_points_b, stationary_points_c)
 
 
 # Second derivatives for Function a
@@ -46,6 +46,7 @@ Hessian_a = Matrix([[second_partial_F_a_x1x1, second_partial_F_a_x1x2],
 
 # Evaluate the Hessian matrix at the stationary point for Function a
 Hessian_a_at_stationary_a = Hessian_a.subs({x1: 2, x2: 1})
+
 
 # Second derivatives for Function b
 second_partial_F_b_x1x1 = diff(partial_F_b_x1, x1)
@@ -65,7 +66,14 @@ F_c_double_prime = diff(F_c_prime, x)
 # Evaluate the second derivative at the stationary points for Function c
 second_derivative_at_stationary_c = [F_c_double_prime.subs(x, point) for point in stationary_points_c]
 
-print(Hessian_a_at_stationary_a, Hessian_b_at_stationary_b, second_derivative_at_stationary_c)
+print("dFax1=",latex(partial_F_a_x1), "dFax2=",latex(partial_F_a_x2))
+print("dFbx2=",partial_F_b_x1, "dFbx2=",partial_F_b_x2)
+print("dFc=",latex(F_c_prime))
+
+print(latex(stationary_points_a), latex(stationary_points_b), latex(stationary_points_c))
+print(latex(Hessian_a),latex(Hessian_b), latex(F_c_double_prime))
+
+print(latex(Hessian_a_at_stationary_a), latex(Hessian_b_at_stationary_b), latex(second_derivative_at_stationary_c))
 
 # Checking the definiteness of the Hessian matrices
 
@@ -83,5 +91,5 @@ def check_definiteness(matrix):
 definiteness_a = check_definiteness(Hessian_a_at_stationary_a)
 definiteness_b = check_definiteness(Hessian_b_at_stationary_b)
 
-definiteness_a, definiteness_b
+print(definiteness_a, definiteness_b)
 
