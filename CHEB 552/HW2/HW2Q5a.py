7263a10bea467bf22e4bf338a6ac4ae6bfe339d1
Conroy <conroycong@outlook.com>
2024-02-27 22:25:45 -0800

    Updated 2.27

--- CHEB 552/HW2/HW2Q5a.py
+++ CHEB 552/HW2/HW2Q5a.py
@@ -17,12 +17,17 @@ partial_x1 = diff(L, x1)
 partial_x2 = diff(L, x2)
 partial_lambda1 = diff(L, lambda1)
 partial_lambda2 = diff(L, lambda2)
-
+second_partial_x1x1 = diff(partial_x1,x1)
+second_partial_x1x2 = diff(partial_x1,x2)
+second_partial_x2x2 = diff(partial_x2,x2)
 # Solve the system of equations
 solution = solve((partial_x1, partial_x2, partial_lambda1, partial_lambda2), (x1, x2, lambda1, lambda2))
-
+H = Matrix([[second_partial_x1x1, second_partial_x1x2], 
+            [second_partial_x1x2, second_partial_x2x2]])
 F_min_value = F.subs([(x1, solution[x1]), (x2, solution[x2])])
-
+# H_eva = H.subs([(x1, solution[x1]), (x2, solution[x2])])
+H_eva = H.subs([(x1, 3), (x2, -1)])
+eigenvalues = H_eva.eigenvals()
 print('F=',latex(F))
 print('L=',latex(L))
 print('partial_x1=',latex(partial_x1 ))
@@ -31,3 +36,5 @@ print('partial_lambda1=',latex(partial_lambda1))
 print('partial_lambda2',latex(partial_lambda2))
 print('solution=',latex(solution))
 print('F_min_value=',latex(F_min_value))
+print('H*=',latex(H_eva))
+print('EV*=',latex(eigenvalues))
--- CHEB 552/HW2/HW2Q5b.py
+++ CHEB 552/HW2/HW2Q5b.py
@@ -18,12 +18,17 @@ partial_x1_new = diff(L_new, x1)
 partial_x2_new = diff(L_new, x2)
 partial_lambda1_new = diff(L_new, lambda1)
 partial_lambda2_new = diff(L_new, lambda2)
-
+second_partial_x1x1 = diff(partial_x1_new,x1)
+second_partial_x1x2 = diff(partial_x1_new,x2)
+second_partial_x2x2 = diff(partial_x2_new,x2)
 # Solve the system of equations for the new problem
 solution_new = solve((partial_x1_new, partial_x2_new, partial_lambda1_new, partial_lambda2_new), (x1, x2, lambda1, lambda2))
-
+H = Matrix([[second_partial_x1x1, second_partial_x1x2], 
+            [second_partial_x1x2, second_partial_x2x2]])
 F_min_value = F_new.subs([(x1, solution_new[x1]), (x2, solution_new[x2])])
+H_eva = H.subs([(x1, solution_new[x1]), (x2, solution_new[x2])])
 
+eigenvalues = H_eva.eigenvals()
 print('F=',latex(F_new))
 print('L=',latex(L_new))
 print('partial_x1=',latex(partial_x1_new ))
@@ -32,3 +37,5 @@ print('partial_lambda1=',latex(partial_lambda1_new))
 print('partial_lambda2',latex(partial_lambda2_new))
 print('solution=',latex(solution_new))
 print('F_min_value=',latex(F_min_value))
+print('H*=',latex(H_eva))
+print('EV*=',latex(eigenvalues))
